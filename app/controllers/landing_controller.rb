b1e2c836ea5962e222c21e44ce6b82f0c3e9305e
gabrielpe <gabriel@psilva.email>
2016-08-27 02:50:26 -0400

    pagination implemented

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,6 +4,7 @@ class LandingController < ApplicationController
 
     @projects = Project.all
     @changes = Change.all
+    @changes_calendar = Change.paginate(page: params[:page], per_page: 5).order('implementation_date DESC')
 
   end
 
