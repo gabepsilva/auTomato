87a929abc6960b852d6600c3903a0425a3e210ae
gabrielpe <gabriel@psilva.email>
2016-08-26 21:11:54 -0400

    Implemented datetime picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require jquery
+//= require moment
+//= require bootstrap-datetimepicker
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
