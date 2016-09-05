10870335408fbc6e58840b54aaa9a5c592393532
gabrielpe <gabriel@psilva.email>
2016-09-05 00:21:01 -0400

    Devise and users 100%

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, length: 0..5, allow_blank: true if false
+
 end
--- db/migrate/20160904160005_devise_create_users.rb
+++ db/migrate/20160904160005_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :name,               null: false, default: ""
+      t.string :contact
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
