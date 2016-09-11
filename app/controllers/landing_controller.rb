cf26503718acb995bb62ba96db0b4baa3be28920
gabrielpe <gabriel@psilva.email>
2016-09-11 13:05:32 -0400

    added eager loading on the landing page - better db performance

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,8 +3,8 @@ class LandingController < ApplicationController
   def index
 
     @projects = Project.all
-    @changes = Change.all
-    @changes_calendar = Change.paginate(page: params[:page], per_page: 5).order('implementation_date DESC')
+    @changes = Change.all.includes(:project).includes(:owner)
+    @changes_calendar = @changes.paginate(page: params[:page], per_page: 5).order('implementation_date DESC')
 
     @landing = Landing.first
     @landing.links_to_display = %i(new_project new_change)
